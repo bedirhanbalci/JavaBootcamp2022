d663ed38aec739dde2eb363df47f0c3fe7b66601
Bedirhan Balcı <61194064+bedirhanbalci@users.noreply.github.com>
2022-10-15 02:35:52 +0300

    1.Week 2.Homework added

---
+++ Week 1/javademos/sesliHarfler/src/sesliHarfler/Main.java
@@ -0,0 +1,34 @@
+package sesliHarfler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char harf = 'e';
+
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+		case 'a':
+		case 'ı':
+		case 'o':
+		case 'u':
+			System.out.println("Kalın sesli harf");
+			break;
+		case 'E':
+		case 'İ':
+		case 'Ö':
+		case 'Ü':
+		case 'e':
+		case 'i':
+		case 'ö':
+		case 'ü':
+			System.out.println("İnce sesli harf");
+			break;
+		default:
+			System.out.println("Yanlış harf");
+		}
+	}
+
+}
